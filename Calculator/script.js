c6750f383d8033d998812d49b5980aeb79459e11
Aashish Kumar Prasad <146658634+AashishKrPd@users.noreply.github.com>
2024-04-19 23:33:30 +0530

    Calculator Updated

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,28 +1,66 @@
+// const display = document.querySelector("#display");
 const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach((item)=>{
     item.onclick = () =>{
         if (item.id == 'clear'){
-            display.innerText = '';
+            display.value = '';
         }
         else if (item.id == 'backspace'){
-            let string = display.innerText.toString();
-            display.innerText = string.substr(0, string.length - 1);
+            let string = display.value.toString();
+            display.value = string.substr(0, string.length - 1);
         }
-        else if (display.innerText != '' && item.id == 'equal'){
-            text = display.innerText.replaceAll("x","*");
-            display.innerText = eval(text);
+        else if (display.value != '' && item.id == 'equal'){
+            text = display.value.replaceAll("^","**");
+            text = text.replaceAll("x","*");
+            display.value = eval(text);
         }
-        else if (display.innerText == '' && item.id == 'equal'){
-            display.innerText = "Empty!";
-            setTimeout(() => (display.innerText = ''), 2000)
+        else if (display.value == '' && item.id == 'equal'){
+            display.value = "Empty!";
+            setTimeout(() => (display.value = ''), 2000);
         }
         else {
-            display.innerText += item.id;
+            display.value += item.id;
         }
     }
 })
+
+document.addEventListener('keydown', function(event) {
+    const key = event.key;
+    
+
+    if (key === 'Enter') {
+        if (display.value == ''){
+            display.value = "Empty!";
+            setTimeout(() => (display.value = ''), 2000);
+        }
+        else{
+            text = display.value.replaceAll("^","**");
+            text = text.replaceAll("x","*");
+            display.value = eval(text);
+        }
+    } 
+
+    else if (key === 'Backspace') {
+        let string = display.value.toString();
+        display.value = string.substr(0, string.length - 1);
+    } 
+    
+    else if (key === 'Delete') {
+        display.value = '';
+    }
+
+    else if (event.shiftKey && event.code === 'Digit5') {     
+        display.value+="%";        
+    }
+
+    else if (event.code.startsWith('Numpad')) {
+        const num = key[key.length - 1];
+        display.value += num;
+    } 
+    
+});
 
 // Toggle Theme 
 
